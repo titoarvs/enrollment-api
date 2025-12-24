179533e5656a0477492d2fdf6323dcc2e9252266
Arvin Ayson <arvin@titosolutions.ph>
2025-12-24 23:01:45 +0800

    Update chat.gateway.ts

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -11,7 +11,7 @@ import { RoomService } from 'src/room/room/room.service';
 
 @WebSocketGateway({
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
     credentials: true,
   },
   transports: ['websocket', 'polling'],
